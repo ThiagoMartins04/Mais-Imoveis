e82d83d8b76b910795cddd5d6105a006d48749e6
ThiagoMartins04 <108444333+ThiagoMartins04@users.noreply.github.com>
2022-08-06 00:03:59 -0300

    Adicionado
    Adicionado efeito no header ao rolar o scroll para baixo e para cima

--- JS/header.js
+++ JS/header.js
@@ -19,3 +19,14 @@ function bgHeader() {
 }
 
 window.addEventListener('scroll', bgHeader)
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-104px";
+  }
+  prevScrollpos = currentScrollPos;
+}
